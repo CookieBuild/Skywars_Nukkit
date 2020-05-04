8ecd45ba7b9a4867950d8d73fd601d4b061f1b0d
Guillaume Claverie <guillaume.claverie@mail.com>
2020-05-04 11:48:53 +0200

    isGameEnded base

--- src/main/java/SkywarsGame.java
+++ src/main/java/SkywarsGame.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import cn.nukkit.Player;
 import cn.nukkit.Server;
 
 public class SkywarsGame extends Game {
@@ -9,6 +10,34 @@ public class SkywarsGame extends Game {
 
     @Override
     public boolean isGameEnded() {
-        return false;
+        if (this.hasStarted() && getPlayers().size() <= 1) {
+
+            for (Player p : this.server.getOnlinePlayers().values()) {
+
+                if (this.plugin.isProxyEnabled) {
+                    ((cbPlayer) p).proxyTransfer("BbLobby-1");
+                } else {
+                    p.kick("End of game.");
+                }
+
+
+            }
+
+            for (Player p : this.server.getOnlinePlayers().values()) {
+                p.kick("End of game.");
+                // We kick again if there are still a proxy player
+            }
+            // Game has ended. Everyone is gone, time to reset
+            this.resetGame();
+
+            // Unload + reload to reset map
+            this.server.unloadLevel(this.server.getLevelByName("game-" + this.gameNumber), true);
+            this.server.loadLevel("game-" + this.gameNumber);
+
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
